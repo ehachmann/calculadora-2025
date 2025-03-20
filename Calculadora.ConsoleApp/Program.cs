c685ffd190b98beecb01d38e37567ed20a32a9c7
ehachmann <ehachmann@hotmail.com>
2025-03-19 22:59:24 -0300

    Configuração do GitHub

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,34 +4,123 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----------------------------");
-            Console.WriteLine("        Calculadora");
-            Console.WriteLine("----------------------------");
 
-            Console.WriteLine("1 - Somar");
-            Console.WriteLine("2 - Subtrair");
-            string opcao = Console.ReadLine();
+            string[] operacoesRealizadas = new string[100];
+            int contador = 0;
 
-            // Somar dois valores
-            Console.Write("Digite o primeiro número: ");
-
-            int primeiroNumero = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Digite o segundo número: ");
-            int segundoNumero = Convert.ToInt32(Console.ReadLine());
-
-            int resultado = 0;
-
-            if (opcao == "1")
+            while (true)
             {
-                resultado = primeiroNumero + segundoNumero;
-            }
-            else if (opcao == "2")
-            {
-                resultado = primeiroNumero - segundoNumero;
-            }
+                Console.Clear();
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("       Calculadora Básica 2025           ");
+                Console.WriteLine("-----------------------------------------");
 
-            Console.WriteLine($"Resultado: {resultado}");
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicar");
+                Console.WriteLine("4 - Dividir");
+                Console.WriteLine("5 - Tabuada");
+                Console.WriteLine("6 - Histórico de Operações");
+                Console.WriteLine("S - Sair");
+                Console.WriteLine("-----------------------------------------");
+
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+                if (opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "5" && opcao != "6" && opcao != "S")
+                {
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                if (opcao == "S")
+                    break;
+
+                else if (opcao == "5")
+                {
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine("               Tabuada                   ");
+                    Console.WriteLine("-----------------------------------------");
+
+                    Console.Write("Digite o número desejado: ");
+                    int numeroTabuada = int.Parse(Console.ReadLine());
+
+                    for (int i = 1; i <= 10; i++)
+                        Console.WriteLine($"{numeroTabuada} x {i} = {numeroTabuada * i}");
+
+                    Console.ReadLine();
+                    continue;
+                }
+
+                else if (opcao == "6")
+                {
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine("         Histórico de Operações          ");
+                    Console.WriteLine("-----------------------------------------");
+
+                    for (int i = 0; i < operacoesRealizadas.Length; i++)
+                    {
+                        if (operacoesRealizadas[i] != null)
+                            Console.WriteLine(operacoesRealizadas[i]);
+                    }
+
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("-----------------------------------------");
+
+                Console.Write("Digite o primeiro número: ");
+                decimal numero1 = Convert.ToDecimal(Console.ReadLine());
+
+                Console.Write("Digite o segundo número: ");
+                decimal numero2 = Convert.ToDecimal(Console.ReadLine());
+
+
+                decimal resultado = 0;
+
+                switch (opcao)
+                {
+                    case "1":
+                        resultado = numero1 + numero2;
+                        operacoesRealizadas[contador] = $"{numero1} + {numero2} = {resultado}";
+                        break;
+                    case "2":
+                        resultado = numero1 - numero2;
+                        operacoesRealizadas[contador] = $"{numero1} - {numero2} = {resultado}";
+                        break;
+                    case "3":
+                        resultado = numero1 * numero2;
+                        operacoesRealizadas[contador] = $"{numero1} x {numero2} = {resultado}";
+                        break;
+                    case "4":
+                        if (numero2 == 0)
+                        {
+                            Console.WriteLine("Não é possível dividir um número por Zero");
+                            Console.ReadLine();
+                            continue;
+                        }
+                        resultado = numero1 / numero2;
+
+                        operacoesRealizadas[contador] = $"{numero1} / {numero2} = {resultado}";
+
+                        break;
+                }
+
+                contador += 1;
+
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine($"Resultado: {resultado}");
+                Console.WriteLine("-----------------------------------------");
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
 
         }
     }
